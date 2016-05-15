63d2f2266d4c22252b7ec47eeddc43d4f70f5c0e
orhanf <orhan.firat@ceng.metu.edu.tr>
2016-05-15 18:23:08 -0400

    add comments and readme for data preprocessing

--- data/merge.sh
+++ data/merge.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+# This script merges all the bitext files in the current directory. 
+# Source side files are concatenated into all_[src]-[trg].[src]
+# Target side files are concatenated into all_[src]-[trg].[trg] 
 
+if [ "$#" -ne 3 ]; then
+    echo ""
+    echo "Usage: $0 src trg path_to_data"
+    echo ""
+    exit 1
+fi
 
 SRC=$1
 TRG=$2
 
-FSRC=all_${1}-${2}.${1}
-FTRG=all_${1}-${2}.${2}
+DATA_DIR=$3
+
+FSRC=${DATA_DIR}/all_${1}-${2}.${1}
+FTRG=${DATA_DIR}/all_${1}-${2}.${2}
 
 echo "" > $FSRC
-for F in *${1}-${2}.${1}
+for F in ${DATA_DIR}/*${1}-${2}.${1}
 do
     if [ "$F" = "$FSRC" ]; then
         echo "pass"
@@ -19,7 +30,7 @@ done
 
 
 echo "" > $FTRG
-for F in *${1}-${2}.${2}
+for F in ${DATA_DIR}/*${1}-${2}.${2}
 do
     if [ "$F" = "$FTRG" ]; then
         echo "pass"
